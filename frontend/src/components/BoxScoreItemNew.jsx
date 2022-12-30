4e96929c6605b70af97b944cf3fbae030301b195
hernangonzalez1 <hernangonzalez1@outlook.com>
2022-12-30 08:17:30 -0400

    Changed buttons and scores

--- frontend/src/components/BoxScoreItemNew.jsx
+++ frontend/src/components/BoxScoreItemNew.jsx
@@ -9,13 +9,13 @@ function BoxScoreItemNew({ boxScore }) {
             <div className="h-84 flex w-64 flex-col items-center justify-center space-y-2 rounded-lg bg-gray-100 md:h-36 md:w-96 md:flex-row md:space-y-0">
                 <div className="flex w-1/2 flex-row justify-center py-2">
                     <div className="flex flex-col items-center justify-center rounded-md p-2">
+                        <div className="text-sm">Home</div>
                         <NBAIcon team={boxScore.home.team} />
                         <div
-                            className={`text-sm ${
-                                boxScore.home.score > boxScore.away.score
-                                    ? "font-extrabold"
-                                    : ""
-                            }`}
+                            className={`text-sm ${boxScore.home.score > boxScore.away.score
+                                ? "font-extrabold"
+                                : ""
+                                }`}
                         >
                             {boxScore.home.score}
                         </div>
@@ -26,13 +26,13 @@ function BoxScoreItemNew({ boxScore }) {
                 </div>
                 <div className="flex w-1/2 flex-row justify-center py-2">
                     <div className="flex flex-col items-center justify-center rounded-md p-2">
+                        <div className="text-sm">Away</div>
                         <NBAIcon team={boxScore.away.team} />
                         <div
-                            className={`text-sm ${
-                                boxScore.away.score > boxScore.home.score
-                                    ? "font-extrabold"
-                                    : ""
-                            }`}
+                            className={`text-sm ${boxScore.away.score > boxScore.home.score
+                                ? "font-extrabold"
+                                : ""
+                                }`}
                         >
                             {boxScore.away.score}
                         </div>
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom"
-import { FaQuestionCircle, FaTicketAlt } from 'react-icons/fa'
+import { FaTicketAlt, FaBasketballBall, FaBox } from 'react-icons/fa'
 
 function Home() {
     return (
@@ -9,13 +9,23 @@ function Home() {
                 <p>Please choose from an option below</p>
             </section>
 
-            <Link to='/new-boxscore' className="btn btn-reverse btn-block">
-                <FaQuestionCircle /> Add a new box score
-            </Link>
+            <div className="flex flex-row space-x-4 justify-center">
+                <button class="flex bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
 
-            <Link to='/boxScores' className="btn btn-block">
-                <FaTicketAlt /> View My Matches
-            </Link>
+                    <Link to='/new-boxscore' >
+                        <FaBasketballBall /> Add a new box score
+                    </Link>
+                </button>
+                <button class="flex bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded">
+                    <Link to='/boxScores'>
+                        <FaBox /> View My Matches
+                    </Link>
+                </button>
+
+            </div>
+
+
+
         </>
     )
 }
