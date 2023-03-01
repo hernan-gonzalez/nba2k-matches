c872a81c47726922d7ecfba39cd40fa628ca6bb0
hernangonzalez1 <hernangonzalez1@outlook.com>
2023-02-28 23:13:02 -0400

    Added playerRecord based off playerRecord endpoint

--- frontend/src/components/BackButton.jsx
+++ frontend/src/components/BackButton.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 export default function BackButton({ url }) {
     return (
         <div className="flex relative">
-            <button class="right-0 bg-gray-400 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded">
+            <button className="right-0 bg-gray-400 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded">
                 <Link to={url}>Back</Link>
             </button>
         </div>
