d73302eeea0027818ddd2d536bb77a1c1e300b35
hernangonzalez1 <hernangonzalez1@outlook.com>
2023-03-04 15:24:51 -0400

    added cors

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ require('colors')
 require('dotenv').config()
 const { errorHandler } = require('./middleware/errorMiddleware')
 const connectDB = require('./config/db')
+const cors = require('cors')
 
 const PORT = process.env.PORT || 5000
 
@@ -11,7 +12,7 @@ const PORT = process.env.PORT || 5000
 connectDB()
 
 const app = express()
-
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
