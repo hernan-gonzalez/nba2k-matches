28d875cb46ebd1e1a2ed853c820178473f1e0f06
hernangonzalez1 <hernangonzalez1@outlook.com>
2023-03-05 13:00:45 -0400

    changed default host

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -1,6 +1,6 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
-const BACKEND_HOST = process.env.REACT_APP_BACKEND_HOST || 'http://localhost';
+const BACKEND_HOST = process.env.REACT_APP_BACKEND_HOST || 'http://localhost:5000';
 
 module.exports = function (app) {
 
