d8c79d97c600c750c5e20d18ce2486efa34c04f2
hernangonzalez1 <hernangonzalez1@outlook.com>
2023-03-05 08:02:29 -0400

    Removed Proxy and api env var

--- frontend/src/features/boxScores/boxScoresService.js
+++ frontend/src/features/boxScores/boxScoresService.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const API_URL = process.env.REACT_APP_API_BASE_URL + '/api/boxscore/'
+const API_URL = '/api/boxscore'
 
 //Create new boxScore
 const createBoxScore = async (boxScoreData, token) => {
