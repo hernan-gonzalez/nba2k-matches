18137f23322f00f7fccc00757510dfc9621cbee9
Hernan Gonzalez <hernan9876@gmail.com>
2023-03-05 11:19:12 -0400

    Update setupProxy.js
    Removed port

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -1,7 +1,6 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const BACKEND_HOST = process.env.REACT_APP_BACKEND_HOST || 'http://localhost';
-const BACKEND_PORT = process.env.REACT_APP_BACKEND_PORT || 5000;
 
 module.exports = function (app) {
 
